f95dee417ddacee4192db7ed8c6ad5df62285918
Praful Surjuse <praful_s21@yahoo.co.in>
2021-08-08 19:23:44 -0500

    drools project gradle

---
+++ boot-drools-gradle/src/main/java/com/codeusingjava/bootdrools/ProcessTest2.java
@@ -0,0 +1,52 @@
+package com.codeusingjava.bootdrools;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jbpm.process.instance.impl.demo.SystemOutWorkItemHandler;
+import org.jbpm.runtime.manager.impl.SimpleRegisterableItemsFactory;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.process.ProcessInstance;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.internal.runtime.manager.RuntimeManagerFactory;
+
+/**
+ * This is a sample file to launch a process.
+ */
+public class ProcessTest2 {
+
+    public static final void main(String[] args) {
+        try {
+        	
+        	
+            // load up the knowledge base
+	        KieServices ks = KieServices.Factory.get();
+    	    KieContainer kContainer = ks.getKieClasspathContainer();
+        	KieSession kSession = kContainer.newKieSession("kbase");
+
+
+            // start a new process instance
+           
+            
+            //kSession.getWorkItemManager().registerWorkItemHandler("Email",
+            //		                                       (WorkItemHandler) new org.jbpm.process.workitem.email.EmailWorkItemHandler("smtp.gmail.com", "587", "prafultest21@gmail.com", "Prafultest@21","true"));
+            Map<String, Object> params = new HashMap<String, Object>();
+            params.put("variable1", "Dia duit Domhanda");
+            params.put("variable2", "Hallo Welt");
+            params.put("variable3", "Bonjour tout le monde");
+            
+
+          //  kSession.startProcess("com.sample.bpmn.hello", params);
+            ProcessInstance processInstance = kSession.startProcess("com.sample.bpmn.hello");
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+
+
+
+}
---
+++ boot-drools-gradle/src/test/java/com/codeusingjava/bootdrools/BootDroolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeusingjava.bootdrools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootDroolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
